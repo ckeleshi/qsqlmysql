b728681c817ee41cc2b85ec4da9e85ae399f4e6a
clanschen <clanschen@gmail.com>
2023-08-15 11:05:56 +0800

    first commit

---
+++ main.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <qsqldriverplugin.h>
+#include <qstringlist.h>
+#include "qsql_mysql_p.h"
+
+QT_BEGIN_NAMESPACE
+
+class QMYSQLDriverPlugin : public QSqlDriverPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QSqlDriverFactoryInterface" FILE "mysql.json")
+
+public:
+    QMYSQLDriverPlugin();
+
+    QSqlDriver* create(const QString &) override;
+};
+
+QT_END_NAMESPACE
